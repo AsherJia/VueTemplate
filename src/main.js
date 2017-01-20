1459eff1c571c6732602a2270aea768c8dc83b1f
jiazw <jiazw@Ctrip.com>
2017-01-20 18:15:16 +0800

    push

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 
 import store from './store'
 import router from './router'
-
-Vue.use(VueResource)
 
 new Vue({ store, router }).$mount('#app')
